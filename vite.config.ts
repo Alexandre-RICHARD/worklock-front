f9aa9e4e18db6345a9cd77fdfb609d76e87b8b27
Alexandre RICHARD <alexandre.richard.dev@gmail.com>
2024-01-17 19:23:27 +0100

    :arrow_up::memo::wrench: Update dependencies, update package.json, add env varible to configurate starting port

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,35 @@
-import {defineConfig} from "vitest/config";
+import {defineConfig, loadEnv} from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-    "resolve": {
-        "alias": {
-            "@": path.resolve(__dirname, "./src"),
-            "@assets": path.resolve(__dirname, "./src/assets"),
-            "@components": path.resolve(__dirname, "./src/components"),
-            "@store": path.resolve(__dirname, "./src/store"),
-            "@slices": path.resolve(__dirname, "./src/store/slices"),
-            "@styles": path.resolve(__dirname, "./src/styles"),
-            "@tests": path.resolve(__dirname, "./src/tests"),
-            "@utilities": path.resolve(__dirname, "./src/utilities"),
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        "server": {"port": parseInt(env.VITE_PORT)},
+        "resolve": {
+            "alias": {
+                "@": path.resolve(__dirname, "./src"),
+                "@assets": path.resolve(__dirname, "./src/assets"),
+                "@components": path.resolve(__dirname, "./src/components"),
+                "@store": path.resolve(__dirname, "./src/store"),
+                "@slices": path.resolve(__dirname, "./src/store/slices"),
+                "@styles": path.resolve(__dirname, "./src/styles"),
+                "@tests": path.resolve(__dirname, "./src/tests"),
+                "@utilities": path.resolve(__dirname, "./src/utilities"),
+            },
         },
-    },
-    "plugins": [react()],
-    "build": {
-        "assetsDir": "./",
-        "outDir": "./build",
-        "cssCodeSplit": false,
-        "rollupOptions": {"input": {"app": "./index.html"}},
-    },
-    "test": {
-        "globals": true,
-        "environment": "jsdom",
-        "setupFiles": ["src/tests/setup.ts"],
-        "env": {"IS_REACT_ACT_ENVIRONMENT": "true"},
-    },
+        "plugins": [react()],
+        "build": {
+            "assetsDir": "./",
+            "outDir": "./build",
+            "cssCodeSplit": false,
+            "rollupOptions": {"input": {"app": "./index.html"}},
+        },
+        "test": {
+            "globals": true,
+            "environment": "jsdom",
+            "setupFiles": ["src/tests/setup.ts"],
+            "env": {"IS_REACT_ACT_ENVIRONMENT": "true"},
+        },
+    };
 });
